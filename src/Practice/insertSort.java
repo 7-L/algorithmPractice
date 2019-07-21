d6aa04fd53ca9eb8ef461ff174cde6afe7efb6e8
hzx <928857063@qq.com>
2019-07-21 17:17:20 +0800

    2019年7月21日 16:04:56 insertSort描述

--- src/Practice/insertSort.java
+++ src/Practice/insertSort.java
@@ -22,6 +22,13 @@ public class insertSort {
     }
 
     private static int[] sort(int[] array) {
+        /*
+        * 首先明确一个最简单的道理，就是对于直接插入排序,首先对于整个数组进行第一步一层循环，
+        * 定义两个局部变量，分别为key 和 pre，首先key 是第一层循环的循环值，pre则为当前第一层循环值减一
+        * 然后进行对比，比较交换，对比循环当前数组中的值和设置为循环体中循环值的前一个值
+        * 最终调换成功后，将pre+1的位置设置为key，因为pre为循环值-1
+        * */
+
         int key;
         int pre;
         for (int i = 1; i < array.length; i++) {
@@ -33,7 +40,6 @@ public class insertSort {
             }
             array[pre + 1] = key;
         }
-
         return array;
     }
 }
