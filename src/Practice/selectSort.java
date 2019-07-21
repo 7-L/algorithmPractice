d3eaea384269cbcc07ec453a34f4dcc335a8d3a4
hzx <928857063@qq.com>
2019-07-21 16:16:44 +0800

    2019年7月21日 16:04:56 selectSort

--- src/Practice/selectSort.java
+++ src/Practice/selectSort.java
@@ -14,4 +14,14 @@ package Practice;
  *@description
  */
 public class selectSort {
+
+
+
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{11, 23, 14, 44, 63, 10, 8, 4, 6, 1};
+        for (int a : sort(arr)) {
+            System.out.println("selectSort---" + a);
+        }
+    }
 }
