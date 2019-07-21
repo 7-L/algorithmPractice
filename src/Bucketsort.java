5b3a23de462fd3275d5b6fff499643a92c141613
hzx <928857063@qq.com>
2019-07-21 18:18:24 +0800

    2019年7月21日 18:18:09 bucketSort

---
+++ src/Bucketsort.java
@@ -0,0 +1,27 @@
+/**
+ * @Project: nowcoder_test
+ * @Package PACKAGE_NAME
+ * @Description: TODO
+ * @author : hzx
+ * @date Date : 2019年07月21日 18:06
+ * @version V1.0
+ */
+
+/**
+ * @author hzx
+ * @createTime 21 18:06
+ * @description
+ */
+public class Bucketsort {
+
+    /*
+     * 时间复杂度O(n),思想都是来自于桶排序，桶排序不是一种具体的排序算法，而是一种思想。
+     * */
+
+    /*
+     * 1、计数排序
+     * 分别创建100-300号的桶，将待排序数按对应的桶号存储，全部存储完毕后，从100号-300号桶逐一倒出数值，即完成排序。
+     * 2、基数排序
+     * 将待排序数列分别按照，个位、十位、百位 添加到不同的桶中并倒出，三次装入与倒出后的数列即为有序数列。
+     * */
+}
