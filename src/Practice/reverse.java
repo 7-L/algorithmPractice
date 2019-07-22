b1765fe2eed5365fcd422eec8665b153576a3df1
hzx <928857063@qq.com>
2019-07-22 14:15:12 +0800

    2019年7月22日 14:15:03

--- src/Practice/reverse.java
+++ src/Practice/reverse.java
@@ -1,4 +1,5 @@
 package Practice;
+
 /**
  * @Project: nowcoder_test
  * @Package Practice
@@ -13,5 +14,20 @@ package Practice;
  *@createTime 22 13:38
  *@description
  */
-public class reverse {
+public class reverse  {
+    public ListNode rList(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+
+        return pre;
+    }
 }
