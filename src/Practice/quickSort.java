77d268d99ac106f7968da32c877d7f2320eb70ce
hzx <928857063@qq.com>
2019-07-23 19:33:26 +0800

    2019年7月23日 19:33:17

--- src/Practice/quickSort.java
+++ src/Practice/quickSort.java
@@ -16,7 +16,7 @@ package Practice;
 public class quickSort {
     public static void main(String[] args) {
         int[] arr = new int[]{11, 23, 14, 44, 63, 10, 8, 4, 6, 1};
-        for (int a : sort(arr, 0, arr.length - 1)) {
+        for (int a : sort2(arr, 0, arr.length - 1)) {
             System.out.println("quickSort2---" + a);
         }
     }
@@ -49,6 +49,37 @@ public class quickSort {
         arr[i] = arr[j];
         arr[j] = temp;
     }
+
+    public static int[] sort2(int[] array, int left, int right) {
+        if (left < right) {
+            int partitionIndex = partition2(array, left, right);
+            sort2(array, left, partitionIndex - 1);
+            sort2(array, partitionIndex + 1, right);
+        }
+        return array;
+    }
+
+    private static int partition2(int[] array, int left, int right) {
+        int pivot = left;
+        int index = pivot + 1;
+        for (int i = index; i <= right; i++) {
+            if (array[i] < array[pivot]) {
+                swap2(array, i, index);
+                index++;
+            }
+
+        }
+        swap2(array, pivot, index - 1);
+        return index - 1;
+    }
+
+    private static void swap2(int[] array, int i, int j) {
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+
 
 
 }
