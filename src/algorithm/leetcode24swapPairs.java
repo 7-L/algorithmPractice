bdfc9c4809be65d95092d784b4f4f89751a46896
7-L <7_ll@sina.com>
2019-08-17 15:29:10 +0800

    2019年08月17日15:03:41

--- src/algorithm/leetcode24swapPairs.java
+++ src/algorithm/leetcode24swapPairs.java
@@ -1,4 +1,21 @@
 package algorithm;
 
+import Practice.ListNode;
+
 public class leetcode24swapPairs {
+    public ListNode swapPairs(ListNode head) {
+        ListNode pre = new ListNode(0);
+        ListNode res = pre;
+        while (head != null && head.next != null) {
+            ListNode next = head.next.next;
+            pre.next = head.next;
+            pre = head;
+            head.next.next = head;
+            head = next;
+        }
+        pre.next = head;
+        return res.next;
+    }
+
+
 }
