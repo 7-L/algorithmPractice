f9de8f880a82d29273397a928eaf165640d372ba
7-L <7_ll@sina.com>
2019-08-17 14:48:18 +0800

    2019年08月17日14:48:12

---
+++ src/algorithm/leetcode206reverse.java
@@ -0,0 +1,20 @@
+package algorithm;
+
+import Practice.ListNode;
+
+public class leetcode206reverse {
+    public ListNode reverse(ListNode head) {
+        if (head == null) {
+            return null;
+        }
+        ListNode pre = null;
+        ListNode next = null;
+        while (head != null) {
+            next = head.next;
+            head.next = pre;
+            pre = head;
+            head = next;
+        }
+        return pre;
+    }
+}
