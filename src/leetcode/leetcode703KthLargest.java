46386e37174d2a9e891ae932661776028d32789d
hzx <928857063@qq.com>
2019-08-20 10:50:39 +0800

    2019年8月20日 10:50:31

--- src/leetcode/leetcode703KthLargest.java
+++ src/leetcode/leetcode703KthLargest.java
@@ -1,9 +1,29 @@
 package leetcode;
+import	java.util.PriorityQueue;
 
 public class leetcode703KthLargest {
     /*
     * 返回数据流中的第K大元素
     * */
 
+    final PriorityQueue<Integer> priorityQueue;
+    final int k;
 
+    public leetcode703KthLargest(int k, int[] a) {
+        this.k = k;
+        priorityQueue = new PriorityQueue<> (k);
+        for (int i : a) {
+            add(i);
+        }
+    }
+
+    public int add(int i) {
+        if (priorityQueue.size() < i) {
+            priorityQueue.offer(i);
+        } else if (priorityQueue.peek() < i) {
+            priorityQueue.poll();
+            priorityQueue.offer(i);
+        }
+        return priorityQueue.peek();
+    }
 }
