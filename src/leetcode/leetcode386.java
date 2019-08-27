62b63675922af0c136a5a1a96f9ff47b1f9fe033
hzx <928857063@qq.com>
2019-08-27 20:23:49 +0800

    2019年8月21日 18:54:05

--- src/leetcode/leetcode386.java
+++ src/leetcode/leetcode386.java
@@ -1,3 +1,5 @@
+
+
 package leetcode;
 /**
  * @Project: nowcoder_test
@@ -8,10 +10,44 @@ package leetcode;
  * @version V1.0
  */
 
+
+import java.util.Arrays;
+
 /**
  * @author hzx
  * @createTime 21 18:53
  * @description lexicalOrder
  */
-public class leetcode386 {
+ class leetcode386 extends superclass {
+    public void showPwrd(String str1, StringBuffer str2) {
+        System.out.println(str1 + "" + str2);
+    }
+
+
+    private String showPefefc() {
+        return "sh";
+    }
+
+    private void show(String str) {
+
+    }
+
+    static int x = 4;
+    public static void main(String[] args) {
+        {
+            int x = 5;
+
+            System.out.println(x);
+        }
+        System.out.println(x);
+    }
+
+}
+class superclass{
+    public String showPwrd(String str1, String str2) {
+        return str1 + str2;
+    }
+    private String showPefefc(){
+        return "showPW";
+    }
 }
