661b3738bc5d57d7a7c04a49859cc96ed7e9bb6f
hzx <928857063@qq.com>
2019-09-02 18:11:04 +0800

    2019年9月2日 18:10:56

---
+++ src/meituan/example.java
@@ -0,0 +1,24 @@
+package meituan;
+/**
+ * @Project: nowcoder_test
+ * @Package meituan
+ * @Description: TODO
+ * @author : hzx
+ * @date Date : 2019年09月02日 18:09
+ * @version V1.0
+ */
+
+/**
+ * @author hzx
+ * @createTime 02 18:09
+ * @description
+ */
+public class example {
+    public static void main(String[] args) {
+        Integer a = new Integer(128);
+
+        Integer b = new Integer(128);
+
+        System.out.println(a.equals(b));
+    }
+}
