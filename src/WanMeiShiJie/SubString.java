ea05688f0e5ecd3aecee1af8c15e15ea9233c319
hzx <928857063@qq.com>
2019-09-16 10:40:54 +0800

    二〇一九年九月十六日 10:40:47

---
+++ src/WanMeiShiJie/SubString.java
@@ -0,0 +1,17 @@
+package WanMeiShiJie;
+/**
+ * @Project: nowcoder_test
+ * @Package WanMeiShiJie
+ * @Description: TODO
+ * @author : hzx
+ * @date Date : 2019年09月16日 10:40
+ * @version V1.0
+ */
+
+/**
+ *@author hzx
+ *@createTime 16 10:40
+ *@description 求字符串的最长重复子串
+ */
+public class SubString {
+}
