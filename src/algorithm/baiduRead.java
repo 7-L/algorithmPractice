f469e1b75aa296f3c056ba1db6d69b6e4ed78369
hzx <928857063@qq.com>
2019-09-20 16:18:05 +0800

    2019年9月20日 16:17:58

--- src/algorithm/baiduRead.java
+++ src/algorithm/baiduRead.java
@@ -8,6 +8,8 @@ package algorithm;
  * @version V1.0
  */
 
+import java.util.HashSet;
+
 /**
  * @author hzx
  * @createTime 17 19:26
@@ -23,6 +25,15 @@ public class baiduRead {
         System.out.println(baiduRead.str + "and");
         System.out.println(baiduRead.chars);
 
+        HashSet<Integer> hashSet = new HashSet<>();
+        hashSet.add(2);
+        hashSet.add(1);
+        hashSet.add(4);
+        hashSet.add(3);
+        hashSet.add(3);
+        for (Integer integer : hashSet) {
+            System.out.println(integer);
+        }
     }
 
     private void change(String str, char[] chars) {
